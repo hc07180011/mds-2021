d25514b5ac4dfe71cfb607d75f9cc10c20dbfb6e
hc07180011 <hendry0718@gmail.com>
2021-12-09 23:41:44 +0800

    2 try

--- hw3/2.py
+++ hw3/2.py
@@ -1,136 +1,89 @@
 import os
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
-from keras import backend as K
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Flatten
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import mobilenet
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix, classification_report
 
 
-cache_dir = ".cache"
-os.makedirs(cache_dir, exist_ok=True)
+def _data_loader(target_shape=(300, 300), batch_size=128, random_seed=42):
+    train_dir = os.path.join("casting_data", "casting_data", "train")
+    test_dir = os.path.join("casting_data", "casting_data", "test")
 
-if not os.path.exists(os.path.join(cache_dir, "casting_300.npz")):
-    data_dir = os.path.join("data", "casting_data", "casting_data")
+    train_generator = ImageDataGenerator(rescale=1./255, validation_split=0.1)
+    test_generator = ImageDataGenerator(rescale=1./255)
 
-    train_dir = os.path.join(data_dir, "train")
-    train_def_dir = os.path.join(train_dir, "def_front")
-    train_ok_dir = os.path.join(train_dir, "ok_front")
-
-    test_dir = os.path.join(data_dir, "test")
-    test_def_dir = os.path.join(test_dir, "def_front")
-    test_ok_dir = os.path.join(test_dir, "ok_front")
-
-    X_train = list()
-    y_train = list()
-
-    for path in os.listdir(train_def_dir):
-        img = cv2.imread(os.path.join(train_def_dir, path))
-        X_train.append(img)
-        y_train.append(1)
-
-    for path in os.listdir(train_ok_dir):
-        img = cv2.imread(os.path.join(train_ok_dir, path))
-        X_train.append(img)
-        y_train.append(0)
-
-    X_train = np.array(X_train)
-    y_train = np.array(y_train)
-
-    X_test = list()
-    y_test = list()
-
-    for path in os.listdir(test_def_dir):
-        img = cv2.imread(os.path.join(test_def_dir, path))
-        X_test.append(img)
-        y_test.append(1)
-
-    for path in os.listdir(test_ok_dir):
-        img = cv2.imread(os.path.join(test_ok_dir, path))
-        X_test.append(img)
-        y_test.append(0)
-
-    X_test = np.array(X_test)
-    y_test = np.array(y_test)
-
-    np.savez(os.path.join(cache_dir, "casting_300"), X_train, y_train, X_test, y_test)
-
-else:
-    _cache = np.load(os.path.join(cache_dir, "casting_300.npz"))
-    X_train, y_train, X_test, y_test = [_cache[k] for k in _cache]
+    training_generator = train_generator.flow_from_directory(
+        directory=train_dir,
+        subset="training",
+        target_size=target_shape,
+        color_mode="grayscale",
+        classes=dict({
+            "ok_front": 0,
+            "def_front": 1
+        }),
+        class_mode="binary",
+        batch_size=batch_size,
+        seed=random_seed
+    )
+    validation_generator = train_generator.flow_from_directory(
+        directory=train_dir,
+        subset="validation",
+        target_size=target_shape,
+        color_mode="grayscale",
+        classes=dict({
+            "ok_front": 0,
+            "def_front": 1
+        }),
+        class_mode="binary",
+        batch_size=batch_size,
+        seed=random_seed
+    )
+    testing_generator = test_generator.flow_from_directory(
+        directory=test_dir,
+        target_size=target_shape,
+        color_mode="grayscale",
+        classes=dict({
+            "ok_front": 0,
+            "def_front": 1
+        }),
+        class_mode="binary",
+        batch_size=batch_size,
+        seed=random_seed,
+        shuffle=False
+    )
+    return (training_generator, validation_generator, testing_generator)
 
 
-def preprocess_X(X):
-    X = X.astype("float32")
-    X /= 255
-    return X
+def _model_builder(input_shape, lr):
+    model = Sequential()
+    model.add(Conv2D(32, 3, activation="relu", padding="same", strides=2, input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=2, strides=2))
+    model.add(Conv2D(64, 3, activation="relu", padding="same", strides=2))
+    model.add(MaxPooling2D(pool_size=2, strides=2))
+    model.add(Flatten())
+    model.add(Dense(128, activation="relu"))
+    model.add(Dense(1, activation="sigmoid"))
+
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=Adam(learning_rate=lr),
+        metrics=["accuracy"]
+    )
+    model.summary()
+    return model
 
 
-X_train = preprocess_X(X_train)
-X_test = preprocess_X(X_test)
+target_shape = (300, 300)
 
-model = Sequential()
-model.add(mobilenet.MobileNet(
-    weights="imagenet", input_shape=(X_train.shape[1:]), include_top=False
-))
-model.add(Flatten())
-model.add(Dense(128, activation="relu"))
-model.add(Dense(1, activation="softmax"))
-print(model.summary())
+training_generator, validation_generator, testing_generator = _data_loader(target_shape=target_shape)
 
-
-def f1_m(y_true, y_pred):
-    def precision_m(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
-    def recall_m(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
-
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
-    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
-
-
-model.compile(
-    loss="binary_crossentropy",
-    optimizer="adam",
-    metrics=["accuracy", f1_m]
+model = _model_builder(target_shape+(1,), 0.001)
+model.fit(
+    training_generator,
+    validation_data=validation_generator,
+    epochs=30,
+    verbose=1
 )
-
-batch_size = 128
-epochs = 5
-
-X_train, X_val, y_train, y_val = train_test_split(
-    X_train, y_train, test_size=0.1,
-    random_state=42
-)
-
-history = model.fit(
-    X_train, y_train,
-    batch_size=batch_size,
-    epochs=epochs,
-    verbose=1,
-    validation_data=(X_val, y_val)
-)
-
-plt.plot(history.history["loss"])
-plt.plot(history.history["val_loss"])
-plt.legend(["loss", "validation loss"])
-plt.savefig("loss.png")
-plt.close()
-
-model.evaluate(X_test, y_test)
-
-y_pred = model.predict(X_test)
-print(accuracy_score(y_test, y_pred))
-print(f1_score(y_test, y_pred))
